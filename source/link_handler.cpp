e313ec83b133162554f6131d22e084e51a518407
Philippe Symons <philippe.symons@zappware.com>
2026-09-07 16:39:52 +0200

    Var reinitialization fix by antoineok.
    dataOutBufferCurrIndex is used by GET_CHECKSUM, but it never gets reset if you trigger the transfer again.
    
    Fix: reinitialize it on ::setup()

--- source/link_handler.cpp
+++ source/link_handler.cpp
@@ -133,6 +133,7 @@ void LinkConnection::setup(const u16 *debug_charset)
   link_cable_array_index = 0;
   writeBufferOffset = 0;
   pccsROMptr = nullptr;
+  dataOutBufferCurrIndex = 0;
 
   linkSPI->activate(LinkSPI::Mode::MASTER_256KBPS);
   linkSPI->setWaitModeActive(false);
